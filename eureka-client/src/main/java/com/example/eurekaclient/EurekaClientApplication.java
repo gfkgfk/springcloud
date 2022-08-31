5fd0de9062fd0bffe8454f0120e02a1b0bdbb42f
gfkgfk <gfk000@126.com>
2022-08-31 15:20:11 +0800

    update structure

--- eureka-client/src/main/java/com/example/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/example/eurekaclient/EurekaClientApplication.java
@@ -1,13 +1,13 @@
-package com.example.springcloud;
+package com.example.eurekaclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringcloudApplication {
+public class EurekaClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringcloudApplication.class, args);
+        SpringApplication.run(EurekaClientApplication.class, args);
     }
 
 }
