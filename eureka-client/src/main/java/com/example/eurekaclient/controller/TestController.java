a25eeb2c71ea2d1a988699681058605ef743d5a4
gfkgfk <gfk000@126.com>
2022-08-31 17:28:46 +0800

    update eureka

---
+++ eureka-client/src/main/java/com/example/eurekaclient/controller/TestController.java
@@ -0,0 +1,31 @@
+package com.example.eurekaclient.controller;
+
+import com.netflix.discovery.DiscoveryClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private Registration client;
+
+    @GetMapping("/info")
+    public String info() {
+        @SuppressWarnings("deprecation")
+        String info = "host：" + client.getHost() + "，service_id：" + client.getServiceId();
+        log.info(info);
+        return info;
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+}
